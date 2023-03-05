7d6e0d8663dd22a9608ebb41bdb5df8c2754948d
Haydon Lam <lamhaydon@gmail.com>
2023-03-05 21:14:07 +0000

    🚀 Remove unnecessary include defaults

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -41,8 +41,3 @@ module.exports = {
     return config;
   },
 };
-
-// _app.js or some other file in as high up in the app (like next's new layouts)
-// this ensures various parts of next.js are imported and "used" somewhere so that they wont be tree shaken out
-// note: this is optional in the latest release, as it is auto-injected by NextFederationPlugin now
-require("@module-federation/nextjs-mf/src/include-defaults");
